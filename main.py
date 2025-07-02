5e18f09fcef5e3b70108b219fc631d7ba41d2b39
Tarek Zehar <tzehar@condor.com>
2025-07-02 08:44:44 +0100

    first test

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world")
+import pygame 
+
+pygame.init()
+screen = pygame.display.set_mode((600,800))
+clock = pygame.time.Clock()
+running = True
+
+while running :
+    screen.fill("red")
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
