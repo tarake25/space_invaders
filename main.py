a763e606204192a9c61b8a95e4c751a307a16e95
Tarek Zehar <tzehar@condor.com>
2025-07-02 09:59:35 +0100

    trying to make a structure

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 import pygame 
 
 pygame.init()
-screen = pygame.display.set_mode((600,800))
+
+# screaan settings
+WIDTH = 800
+HIGHT = 600
+
+class game_ob():
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+    def draw (self,screen,w,h):
+        pygame.draw.rect(screen,'red',(self.x,self.y,w,h))
+invader = []
+for i in range(100,700,20):
+    for j in range (40,400,20):
+      invader.append(game_ob(i,j))
+
+
+
+
+screen = pygame.display.set_mode((WIDTH,HIGHT))
+pygame.display.set_caption("space invader")
 clock = pygame.time.Clock()
 running = True
 
 while running :
-    screen.fill("red")
+    screen.fill("black")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    player = game_ob(WIDTH/2-40,HIGHT-40)
+    player.draw(screen,40,20)
 
+    for i in invader :
+        i.draw(screen,15,15)
     pygame.display.flip()
     clock.tick(60)
 
