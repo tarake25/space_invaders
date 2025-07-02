580bdae10f8db49ddc8a1b34264296bf4345b206
Tarek Zehar <tzehar@condor.com>
2025-07-02 13:42:01 +0100

    adding movment

--- main.py
+++ main.py
@@ -12,10 +12,32 @@ class game_ob():
         self.y = y
     def draw (self,screen,w,h):
         pygame.draw.rect(screen,'red',(self.x,self.y,w,h))
+
+
+class player_class(game_ob):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+    def move(self, speed):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT]:
+            self.x += speed
+        if keys[pygame.K_LEFT]:
+            self.x -= speed
+
+class inv(game_ob):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+    def move(self, speed):
+        if self.x ==500:
+            self.x += speed
+            pass
+        self.x -= speed
+
+
 invader = []
-for i in range(100,700,20):
-    for j in range (40,400,20):
-      invader.append(game_ob(i,j))
+for i in range(100,700,40):
+    for j in range (40,300,30):
+      invader.append(inv(i,j))
 
 
 
@@ -24,18 +46,22 @@ screen = pygame.display.set_mode((WIDTH,HIGHT))
 pygame.display.set_caption("space invader")
 clock = pygame.time.Clock()
 running = True
-
+player = player_class(WIDTH/2-40,HIGHT-40)
 while running :
     screen.fill("black")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    player = game_ob(WIDTH/2-40,HIGHT-40)
+
+
+
+    player.move(10)
     player.draw(screen,40,20)
 
     for i in invader :
-        i.draw(screen,15,15)
+        i.move(5)
+        i.draw(screen,20,20)
     pygame.display.flip()
     clock.tick(60)
 
