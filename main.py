f0aa4a97a391c04abbe688754248f018c546ea7f
Tarek Zehar <tzehar@condor.com>
2025-07-03 10:52:35 +0100

    trying puting a bolit system

--- main.py
+++ main.py
@@ -1,37 +1,63 @@
 import pygame 
 
 pygame.init()
-
+import math
 # screaan settings
 WIDTH = 800
 HIGHT = 600
 
 class game_ob():
+    WIDTH = 800
+    HIGHT = 600
     def __init__(self,x,y):
         self.x = x
         self.y = y
     def draw (self,screen,w,h):
         pygame.draw.rect(screen,'red',(self.x,self.y,w,h))
 
+class bolit(game_ob):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.s = 10
+    def move(self):
+        self.y -= self.s
+    def hit(self,inv):
+        if math.sqrt((self.x - inv.x)**2 + (self.y - inv.y)**2) == 10:
+            return True
+
+            
 
 class player_class(game_ob):
     def __init__(self, x, y):
         super().__init__(x, y)
+        self.b_list = []
     def move(self, speed):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_RIGHT]:
             self.x += speed
         if keys[pygame.K_LEFT]:
             self.x -= speed
+    def shot(self,screen,inv_list):
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            self.b_list.append(bolit(self.x,self.y))
+        for i in range(len(self.b_list)):
+            self.b_list[i].move()
+            self.b_list[i].draw(screen,5,5)
+            for j in range(len(inv_list)):
+                if self.b_list[i].hit(inv_list[j]):
+                    inv_list[j].x += 100000
+                    self.b_list[i].x+= 100000
+                    
+        
 
 class inv(game_ob):
     def __init__(self, x, y):
         super().__init__(x, y)
     def move(self, speed):
-        if self.x ==500:
-            self.x += speed
-            pass
-        self.x -= speed
+        pass
+
 
 
 invader = []
@@ -58,6 +84,7 @@ while running :
 
     player.move(10)
     player.draw(screen,40,20)
+    player.shot(screen,invader)
 
     for i in invader :
         i.move(5)
